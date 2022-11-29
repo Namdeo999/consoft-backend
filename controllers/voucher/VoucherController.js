c9c8dc0461083c45160d5ebaf7bed95393963a36
Intoloshubham <shubham.sahu@intoloindia.com>
2022-11-29 16:13:09 +0530

    "11 29 2022 5th cmt"

--- controllers/voucher/VoucherController.js
+++ controllers/voucher/VoucherController.js
@@ -111,6 +111,102 @@ const VoucherController = {
       return next(error);
     }
   },
+  async getAllVoucher(req, res, next) {
+    let documents;
+    try {
+      documents = await voucher.aggregate([
+        {
+          $match: {
+            company_id: ObjectId(req.params.company_id),
+            voucher_date: req.params.date,
+          },
+        },
+        {
+          $lookup: {
+            from: "voucherDetails",
+            localField: "_id",          
+            foreignField: "voucher_id",
+            as: "voucherData",
+          },
+        },
+        {
+          $unwind: "$voucherData",
+        },
+        {
+          $lookup: {
+            from: "items",
+            localField: "voucherData.item_id",
+            foreignField: "_id",
+            as: "itemData",
+          },
+        },
+        {
+          $unwind: "$itemData",
+        },
+        {
+          $lookup: {
+            from: "units",
+            localField: "voucherData.unit_id",
+            foreignField: "_id",
+            as: "unitData",
+          },
+        },
+        {
+          $unwind: "$unitData",
+        },
+        {
+          $lookup: {
+            from: "projects",
+            localField: "project_id",
+            foreignField: "_id",
+            as: "projectData",
+          },
+        },
+        {
+          $unwind: "$projectData",
+        },
+        {
+          $group: {
+            _id: "$project_id",
+            company_id: { $first: "$company_id" },
+            project_id: { $first: "$project_id" },
+            project_name: { $first: "$projectData.project_name" },
+            voucherData: {
+              $push: {
+                _id: "$voucherData._id",
+                voucher_id: "$voucherData.voucher_id",
+                project_id: "$project_id",
+                voucher_type: "$voucherData.voucher_type",
+                verify_status: "$voucherData.verify_status",
+                revert_status: "$voucherData.revert_status",
+                item_id: "$voucherData.item_id",
+                unit_name: "$unitData.unit_name",
+                voucher_date: "$voucher_date",
+                item_name: "$itemData.item_name",
+                qty: "$voucherData.qty",
+                vehicle_no: "$voucherData.vehicle_no",
+                location: "$voucherData.location",
+                remark: "$voucherData.remark",
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: "$_id",
+            verify_status: "$verify_status",
+            company_id: "$company_id",
+            // project_id: "$project_id",
+            project_name: "$project_name",
+            voucherData: "$voucherData",
+          },
+        },
+      ]);
+      return res.json({ status: 200, data: documents });
+    } catch (error) {
+      return next(error);
+    }
+  },
   async filterVoucher(req, res, next) {
     let documents;
     try {
@@ -165,6 +261,17 @@ const VoucherController = {
         },
         {
           $lookup: {
+            from: "units",
+            localField: "voucherData.unit_id",
+            foreignField: "_id",
+            as: "unitData",
+          },
+        },
+        {
+          $unwind: "$unitData",
+        },
+        {
+          $lookup: {
             from: "projects",
             localField: "project_id",
             foreignField: "_id",
@@ -189,6 +296,7 @@ const VoucherController = {
                 verify_status: "$voucherData.verify_status",
                 revert_status: "$voucherData.revert_status",
                 item_id: "$voucherData.item_id",
+                unit_name: "$unitData.unit_name",
                 voucher_date: "$voucher_date",
                 item_name: "$itemData.item_name",
                 qty: "$voucherData.qty",
